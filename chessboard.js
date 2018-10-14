8758347651ccdb404ea2d2b00619ca95ec56603e
Vatsal Parmar <vparmar@sfu.ca>
2018-10-13 22:35:41 -0700

    Add files via upload

---
+++ chessboard.js
@@ -0,0 +1,18 @@
+
+let odd = " #";
+let even = "# ";
+
+let chessboard = "";
+
+let size = 8;
+
+for (let i = 1; i <= size; i++){
+  if (i % 2 == 0){
+    chessboard = even.repeat(size/2) + "\n";
+  }
+  else{
+    chessboard = odd.repeat(size/2) + "\n";
+  }
+
+  console.log(chessboard);
+}
