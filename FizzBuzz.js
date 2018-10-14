9babb39ba9d3a4167d2886eeb98eed96974ae168
Vatsal Parmar <vparmar@sfu.ca>
2018-10-13 22:34:36 -0700

    Add files via upload

---
+++ FizzBuzz.js
@@ -0,0 +1,12 @@
+
+for (let i = 1; i <=100; i++){
+  if(i % 3 == 0){
+    console.log("Fizz");
+    continue;
+  }
+  else if( i % 5 == 0){
+    console.log("Buzz");
+    continue;
+  }
+  console.log(i);
+}
